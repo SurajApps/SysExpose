175edcf1592283322126912b2ca83218b828cd0b
KINGAWESOME266 <26085740+KINGAWESOME266@users.noreply.github.com>
2020-07-30 17:27:02 +0100

    Added help argument function

--- SysExpose/SysExpose.py
+++ SysExpose/SysExpose.py
@@ -1,18 +1,31 @@
 import sys
-import psutil
-import platform
-from pathlib import *
-from datetime import datetime
+# import psutil
+# import platform
+# from pathlib import *
+# from datetime import datetime
 
 action = sys.argv[1]
 
 
 def Menu():
     if(action == "help"):
-        print("Help")
+        Help_page()
     elif(action == "public"):
         print("Public")
     elif(action == "private"):
         print("Private")
     elif(action == "system"):
         print("System")
+
+
+def Help_page():
+    print('''
+    ------------------------------------------------------------------
+    help = Print this help dialog
+    public = Find the public IP address of this system
+    private = Find the private IP address of this system
+    system = Display system information, such as operating system etc.
+    ------------------------------------------------------------------''')
+
+
+Menu()
