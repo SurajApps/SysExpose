2158e9cced793e9512a73779f5c666923dbd1bf9
KINGAWESOME266 <26085740+KINGAWESOME266@users.noreply.github.com>
2020-07-30 17:21:38 +0100

    Added ability for CLI arguments to be used.

--- SysExpose/SysExpose.py
+++ SysExpose/SysExpose.py
@@ -1,6 +1,18 @@
-import os
+import sys
 import psutil
 import platform
 from pathlib import *
 from datetime import datetime
 
+action = sys.argv[1]
+
+
+def Menu():
+    if(action == "help"):
+        print("Help")
+    elif(action == "public"):
+        print("Public")
+    elif(action == "private"):
+        print("Private")
+    elif(action == "system"):
+        print("System")
